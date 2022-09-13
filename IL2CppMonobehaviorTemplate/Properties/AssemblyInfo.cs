329f7d36a5bbfdb5a95c27188a7d70d1ae5e8e65
SpockBauru <67930504+SpockBauru@users.noreply.github.com>
2022-09-13 19:50:46 -0300

    Added Monobehavior Template

---
+++ IL2CppMonobehaviorTemplate/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using static IL2CppMonobehaviorTemplate.MonoTemplate;
+
+[assembly: AssemblyTitle(GUID)]
+[assembly: AssemblyDescription(PluginName)]
+[assembly: AssemblyProduct(GUID)]
+[assembly: AssemblyVersion(PluginVersion)]
+[assembly: AssemblyFileVersion(PluginVersion)]
