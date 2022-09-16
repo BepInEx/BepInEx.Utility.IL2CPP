9d471cab25ed6611d75dc64119fd15bd0ca464d8
SpockBauru <67930504+SpockBauru@users.noreply.github.com>
2022-09-16 18:28:41 -0300

    Moved IL2CPP template to its own repository

--- IL2CppMonobehaviorTemplate/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using static IL2CppMonobehaviorTemplate.MonoTemplate;
-
-[assembly: AssemblyTitle(GUID)]
-[assembly: AssemblyDescription(PluginName)]
-[assembly: AssemblyProduct(GUID)]
-[assembly: AssemblyVersion(PluginVersion)]
-[assembly: AssemblyFileVersion(PluginVersion)]
