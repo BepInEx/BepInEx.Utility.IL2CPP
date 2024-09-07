37e79e1389b5bc0369a814c0da12cb52f6f8169e
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2024-09-07 19:52:58 +0200

    Add ProcessorAffinityOverride Patcher (#3)

---
+++ Patcher_ProcessorAffinityOverride_IL2CPP_net6/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using BepInEx;
+
+[assembly: AssemblyTitle(ProcessorAffinityOverridePatcher.PatcherName)]
+[assembly: AssemblyProduct(ProcessorAffinityOverridePatcher.GUID)]
+[assembly: AssemblyDescription("BepInEx IL2CPP preloader patcher that finds and replaces a byte pattern in current process memory.")]
+
+[assembly: AssemblyVersion(ProcessorAffinityOverridePatcher.PatcherVersion)]
+[assembly: AssemblyFileVersion(ProcessorAffinityOverridePatcher.PatcherVersion)]
